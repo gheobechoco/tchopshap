057983bf158b9d44168b1cb73ac4c6766c1ab0fd
GeoDev <moueletgeoff@gmail.com>
2025-03-30 12:09:43 +0100

    feat: ajout du système de routing et création de la page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>Bienvenue sur TchôpShap !</h1>
+        <p>Cette page est la page d'accueil de l'application.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
